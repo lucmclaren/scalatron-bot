b4854e30adeb090ebe482fb292edf36ca5cafa9f
Andreas Schroeder <aschroeder@autoscout24.com>
2016-09-16 12:29:21 +0200

    adding base framework

--- src/main/scala/Bot.scala
+++ src/main/scala/Bot.scala
@@ -1,7 +1,13 @@
 class ControlFunctionFactory {
-    def create = new ControlFunction().respond _
+  def create = new ControlFunction().respond _
 }
 
 class ControlFunction {
-    def respond(input: String) = "Status(text=Hello World)"
+  def respond(input: String): String = {
+    val (opcode, paramMap) = CommandParser(input)
+
+    if( opcode == "React" ) {
+      "Status(text=Hello World!)"
+    } else ""
+  }
 }
