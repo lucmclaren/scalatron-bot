09c402a2f541863e0f39a75c18d337ff6f884abe
lmclaren <lmclaren@autoscout24.com>
2016-10-18 17:58:51 +0200

    Bot is now going away from bad things and collecting good things

--- src/main/scala/Bot.scala
+++ src/main/scala/Bot.scala
@@ -1,7 +1,7 @@
 //import com.sun.xml.internal.ws.handler.HandlerProcessor.Direction
 import scala.util.Random
 class ControlFunctionFactory {
-  def create = new ControlFunction().respond _
+  def create = try { new ControlFunction().respond _ } catch { case e: Throwable => e.printStackTrace(); "" }
 }
 
 class ControlFunction {
@@ -9,34 +9,64 @@ class ControlFunction {
     val (opcode, paramMap) = CommandParser(input)
 
     if( opcode == "React" ) {
-      var direction = if(Bot.lastDirection != XY.Zero) Bot.lastDirection else XY.Up
-      val analyzer =  new ViewAnalyzer(new View(paramMap("view")))
-      val wallsAround = analyzer.isWallAround
+      try {
+        var direction = if (Bot.lastDirection != XY.Zero) Bot.lastDirection else XY.Up
+        val view = new View(paramMap("view"))
+        val analyzer = new ViewAnalyzer(view)
 
-      if(analyzer.isWallInDirection(direction)) {
-        val possibleDirections = wallsAround.filter({ case (x,y) => !y }).keys.toArray
-        val randElement = Random.nextInt(possibleDirections.size)
-        direction = possibleDirections(randElement)
+        val offsetFluppet = view.offsetToNearest(Rules.Fluppet)
+        val offsetZugar = view.offsetToNearest(Rules.Zugar)
+
+        (offsetFluppet, offsetZugar) match {
+          case (Some(f), Some(z)) if (z.stepsTo(view.center) >= f.stepsTo(view.center)) => direction = f.signum
+          case (_, Some(z))     => direction = z.signum
+          case (Some(f), None)  => direction = f.signum
+          case (None, None)     => direction = Bot.lastDirection
+        }
+
+        val badObjAround = analyzer.isBadObjAround
+        if (analyzer.isBadObjInDirection(direction)) {
+          val possibleDirections = badObjAround.filter({ case (x, y) => !y }).keys.toArray
+          val randElement = Random.nextInt(possibleDirections.size)
+          direction = possibleDirections(randElement)
+        }
+
+        Bot.lastDirection = direction
+
+        "Move(direction=" + direction.toString + ")"
       }
-      Bot.lastDirection = direction
-      "Move(direction=" + direction.toString + ")|Status(text=No Wall in Sight!)"
-
-    } else "Move(direction=1:0)"
+      catch {
+        case e: Exception => "Log(text=" + e.printStackTrace + ")"
+      }
+    } else ""
   }
 }
 
 // Bot State
 object Bot {
-  var lastDirection = XY.Zero
+  var lastDirection   = XY.Zero
   var wallInDirection = false
 }
 
 object Rules {
-  val Wall = 'W'
+  val Wall      = 'W'
+  val Master    = 'M'
+  val Enemy     = 'm'
+  val Zugar     = 'P'
+  val Toxifera  = 'p'
+  val Fluppet   = 'B'
+  val Snorg     = 'b'
+  val Slave     = 'S'
+  val Minibot   = 's'
+
+  val BadObj = List(Wall, Enemy, Toxifera, Snorg, Minibot)
 }
 
 // Class for analyzing anything view related
 class  ViewAnalyzer(view: View) {
-  def isWallInDirection(direction: XY) : Boolean = view.cellAtRelPos(direction) == Rules.Wall
-  def isWallAround : Map[XY,Boolean] = XY.directions.map { d => d -> isWallInDirection(d) }.toMap
+  def isInDirection(direction: XY, rule: Char) : Boolean = view.cellAtRelPos(direction) == rule
+  def isAround(rule: Char) : Map[XY,Boolean] = XY.directions.map { d => d -> isInDirection(d, rule) }.toMap
+
+  def isBadObjInDirection(direction: XY) : Boolean = Rules.BadObj.contains(view.cellAtRelPos(direction))
+  def isBadObjAround : Map[XY, Boolean] = XY.directions.map { d => d ->  isBadObjInDirection(d)}.toMap
 }
